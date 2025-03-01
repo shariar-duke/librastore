e06faca86ff167c769b2374114cdff1095f99f4c
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-02 00:56:54 +0600

    changes in NavLinks

--- src/app/ui/sidebar/NavLinks.js
+++ src/app/ui/sidebar/NavLinks.js
@@ -27,15 +27,15 @@ const links = [
 
 export default function NavLinks() {
   return (
-    <nav className="flex flex-col gap-2 w-full">
-      {links.map(({ name, href, icon }) => (
+    <nav className="flex flex-row md:flex-col gap-2 w-full">
+      {links.map(({ name, href, icon: Icon }) => (
         <Link
           key={name}
           href={href}
           className="flex items-center gap-3 p-3 text-sm font-medium rounded-md bg-gray-50 hover:bg-sky-100 hover:text-purple-600 transition-all duration-200 ease-in-out"
         >
           {/* for icon */}
-          <icon className="w-6" />
+          <Icon className="w-6" />
 
           {/* for label  */}
           <span className="hidden md:inline-block">{name}</span>

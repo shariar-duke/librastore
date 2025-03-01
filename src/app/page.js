da0c19a70c1ac2c727c02358e6db361b68030496
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-01 19:26:36 +0600

    home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,32 @@
+import { SparklesIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import Link from "next/link";
 import backgroundImage from "../../public/home/book-bg.png";
 export default function Home() {
   return (
-    <div>
+    <div className="relative w-full h-screen flex flex-col justify-center items-center">
       <Image
+        className="blur-sm"
         src={backgroundImage}
-        fill
         alt="Background Image"
-        size="100vw"
+        fill
         style={{ objectFit: "cover" }}
       />
+      <div className="flex flex-col justify-center items-center bg-purple-100 rounded-md p-2 z-10 lg:w-2/5 h-1/4">
+        <div className="flex pb-5 mb-5 text-5xl text-purpule-800">
+          <SparklesIcon className="w-12 h-12" />
+          <p>Litloop</p>
+        </div>
+        <div>
+          <Link
+            className="bg-purple-700 text-white rounded-sm p-2"
+            href="/store"
+          >
+            Take me to the Store
+          </Link>
+        </div>
+      </div>
+
       {/* 
       I can write this in this syntax as well  */}
 

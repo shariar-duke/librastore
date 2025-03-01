489436bec218d39647f4f293c61a80dfa789eca4
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-01 23:47:47 +0600

    initial store layout

---
+++ src/app/store/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "librastore",
+  description: "A Book Store Library",
+};
+
+export default function BookStoreLayout({ children }) {
+  return (
+    <div className="flex xs:flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row">
+      <div className="flex flex-wrap p-2 justify-center">{children}</div>
+    </div>
+  );
+}

69b41f58af10273a19608566064e309f7388ffb7
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-02 15:13:07 +0600

    BookList layout chagnes

--- src/app/ui/books/BookList.js
+++ src/app/ui/books/BookList.js
@@ -2,7 +2,7 @@ import BookCard from "./BookCard";
 
 export default function BookList({ books }) {
   return (
-    <div className="grid grid-cols-4 gap-4">
+    <div className="grid md:grid-col-3 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       {books.map((book) => {
         return <BookCard key={book.id} book={book} />;
       })}

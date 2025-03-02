b5e990274d82d413d95c7671f730e6608f9f7769
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-02 15:51:28 +0600

    toast introduced in root level

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,6 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ToastContainer />
         {children}
       </body>
     </html>

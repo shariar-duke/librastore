2da84d3d8c87c6054046ba8a926aa7e2b592fb5b
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-02 15:02:24 +0600

    BookCard Section added in BookList

--- src/app/ui/books/BookList.js
+++ src/app/ui/books/BookList.js
@@ -2,7 +2,7 @@ import BookCard from "./BookCard";
 
 export default function BookList({ books }) {
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-4 gap-4">
       {books.map((book) => {
         return <BookCard key={book.id} book={book} />;
       })}

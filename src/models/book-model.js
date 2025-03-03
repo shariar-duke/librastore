909fd9b34411585ba22ee5dfb92982620e7ebe00
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-03 14:02:13 +0600

    book schema and model cerated

---
+++ src/models/book-model.js
@@ -0,0 +1,59 @@
+import { Schema } from "mongoose";
+
+const bookSchema = new Schema({
+  title: {
+    required: true,
+    type: String,
+  },
+  description: {
+    required: true,
+    type: String,
+  },
+  author: {
+    required: true,
+    type: String,
+  },
+  cover: {
+    required: true,
+    type: String, // URL as a string
+  },
+  genre: {
+    required: true,
+    type: String,
+  },
+  pages: {
+    required: true,
+    type: Number,
+  },
+  isRented: {
+    required: true,
+    type: Boolean,
+  },
+  stock: {
+    required: true,
+    type: Number,
+  },
+  rentPrice: {
+    required: true,
+    type: Number,
+  },
+  sellPrice: {
+    required: true,
+    type: Number,
+  },
+  sold: {
+    required: true,
+    type: Number,
+  },
+  ISBN: {
+    required: true,
+    type: String,
+  },
+  renterIds: {
+    type: Array,
+    ref: "User",
+  },
+});
+
+export const bookModel =
+  mongoose.models.book ?? mongoose.model("books", bookSchema);

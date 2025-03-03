a139cc903f1ea4cdac88a726482aef0cf3664a28
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-03 14:40:50 +0600

    mongo defatlut id removing util fucniton

---
+++ src/utils/data-util.js
@@ -0,0 +1,12 @@
+export const replaceMongoIdInArray = (array) => {
+  const mappedArray = array
+    .map((item) => {
+      return {
+        ...item, // Spread the current item (e.g., a MongoDB document)
+        id: item._id.toString(), // Add a new field `id` with the string representation of _id
+      };
+    })
+    .map(({ _id, ...rest }) => rest); // Remove the _id field and keep the rest of the fields
+
+  return mappedArray; // Return the new array
+};
